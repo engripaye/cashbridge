4e33f299f30b3948e6955b088a4fe7ed0dbf2df1
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-10 09:59:57 +0100

    init commit by ENGR. IPAYE

---
+++ cashbridge-backend/src/main/java/dev/engripaye/cashbridgebackend/CashbridgeBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.engripaye.cashbridgebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashbridgeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashbridgeBackendApplication.class, args);
+    }
+
+}
---
+++ cashbridge-backend/src/test/java/dev/engripaye/cashbridgebackend/CashbridgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.engripaye.cashbridgebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashbridgeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
