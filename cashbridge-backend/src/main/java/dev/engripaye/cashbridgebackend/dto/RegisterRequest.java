712a0864d0be1bfb439dd6ca430e0e453a3d0c6d
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-12 12:49:10 +0100

    backend service code commit by ENGR. IPAYE

--- cashbridge-backend/src/main/java/dev/engripaye/cashbridgebackend/dto/RegisterRequest.java
+++ cashbridge-backend/src/main/java/dev/engripaye/cashbridgebackend/dto/RegisterRequest.java
@@ -1,4 +1,14 @@
 package dev.engripaye.cashbridgebackend.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+
+    private String fullName;
+    private String email;
+    private String phone;
+    private String nin;
+    private String password;
+
 }
