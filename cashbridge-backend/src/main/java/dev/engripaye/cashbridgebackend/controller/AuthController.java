15695e9c98178c6dcdf25a3d90278c826b681829
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-12 20:34:35 +0100

    backend service code commit by ENGR. IPAYE

--- cashbridge-backend/src/main/java/dev/engripaye/cashbridgebackend/controller/AuthController.java
+++ cashbridge-backend/src/main/java/dev/engripaye/cashbridgebackend/controller/AuthController.java
@@ -1,11 +1,14 @@
 package dev.engripaye.cashbridgebackend.controller;
 
 
+import dev.engripaye.cashbridgebackend.dto.AuthResponse;
+import dev.engripaye.cashbridgebackend.dto.LoginRequest;
+import dev.engripaye.cashbridgebackend.dto.RegisterRequest;
+import dev.engripaye.cashbridgebackend.security.JwtUtil;
 import dev.engripaye.cashbridgebackend.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -13,7 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final JwtUtil jwtUtil;
 
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
+        AuthResponse response = authService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@ResponseBody LoginRequest request){
+
+    }
 
 
 
