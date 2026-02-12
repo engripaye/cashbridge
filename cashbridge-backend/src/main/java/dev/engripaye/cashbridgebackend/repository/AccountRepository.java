4a17cfbc22cf85717f28946ddcd147544eae5321
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-12 12:37:44 +0100

    backend service code commit by ENGR. IPAYE

---
+++ cashbridge-backend/src/main/java/dev/engripaye/cashbridgebackend/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package dev.engripaye.cashbridgebackend.repository;
+
+import dev.engripaye.cashbridgebackend.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByAccountNumber(String accountNumber);
+}
