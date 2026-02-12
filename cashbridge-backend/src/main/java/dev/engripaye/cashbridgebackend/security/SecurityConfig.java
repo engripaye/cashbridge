42be2416ddadbc73d272ce3999d484942dd1cf7b
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-12 11:28:01 +0100

    backend service code commit by ENGR. IPAYE

--- cashbridge-backend/src/main/java/dev/engripaye/cashbridgebackend/security/SecurityConfig.java
+++ cashbridge-backend/src/main/java/dev/engripaye/cashbridgebackend/security/SecurityConfig.java
@@ -1,4 +1,31 @@
 package dev.engripaye.cashbridgebackend.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login", "/register", "/css/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/dashboard", true)
+                        .permitAll())
+                .logout(logout -> logout.logoutSuccessUrl("/login"));
+
+        return httpSecurity.build();
+    }
 }
+
