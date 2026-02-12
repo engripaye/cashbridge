019e46703300f11d837426b05c417d08c3df99cc
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-12 14:21:32 +0100

    backend service code commit by ENGR. IPAYE

--- cashbridge-backend/src/main/java/dev/engripaye/cashbridgebackend/security/SecurityConfig.java
+++ cashbridge-backend/src/main/java/dev/engripaye/cashbridgebackend/security/SecurityConfig.java
@@ -1,12 +1,15 @@
 package dev.engripaye.cashbridgebackend.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
 
 
     @Bean
