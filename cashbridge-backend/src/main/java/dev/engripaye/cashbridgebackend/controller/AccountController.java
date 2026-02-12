cf29a67b3934b7b25fc05166d689036db05038f8
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-12 20:46:12 +0100

    backend service code commit by ENGR. IPAYE

--- cashbridge-backend/src/main/java/dev/engripaye/cashbridgebackend/controller/AccountController.java
+++ cashbridge-backend/src/main/java/dev/engripaye/cashbridgebackend/controller/AccountController.java
@@ -1,4 +1,24 @@
 package dev.engripaye.cashbridgebackend.controller;
 
+import dev.engripaye.cashbridgebackend.entity.Account;
+import dev.engripaye.cashbridgebackend.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/account")
+@RequiredArgsConstructor
 public class AccountController {
+
+
+    private final AccountService accountService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Account> getAccount(@PathVariable Long userId){
+        return ResponseEntity.ok(accountService.getAccount(userId));
+    }
 }
